1e47f36ecae286ba79b8f047c5d9fd7e0aaeb7fb
Maxwell Nganyadi <maxwelltechmaster@outlook.com>
2021-03-03 19:37:48 -0600

    add nth_fibonacci subparser and subcommand

--- euler2/sum_of_even_fibs.py
+++ euler2/sum_of_even_fibs.py
@@ -1,4 +1,5 @@
 """Module that calculates the sum of whatever fibonacci numbers you care about"""
+import argparse
 
 def fibonacci():
     last, current = 0, 1
@@ -6,6 +7,12 @@ def fibonacci():
     for i in range(2, 10):
         last, current = current, current+last
         print(f"Fib({i}) is {current}")
+
+def nth_fibonacci(args):
+    last, current = 0, 1
+    for i in range(2, args.n+1):
+        last, current = current, current+last
+    print(current)
 
 def sum_of_even_fib_nums(upperbound):
     sum = 0
@@ -17,4 +24,12 @@ def sum_of_even_fib_nums(upperbound):
     return sum
 
 if __name__ == "__main__":
-    pass
+    parser = argparse.ArgumentParser(description="Module with lots of Fibonacci tools.")
+    subparsers = parser.add_subparsers()
+
+    parser_nth_fibonacci = subparsers.add_parser('nth_fibonacci', help='Given n, returns F(n).')
+    parser_nth_fibonacci.add_argument('n', type=int)
+    parser_nth_fibonacci.set_defaults(func=fibonacci)
+
+    args = parser.parse_args()
+    args.func(args)
