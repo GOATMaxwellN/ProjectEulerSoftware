12613d216edc6dc51588e2c9535ea7a5fc0d4358
Maxwell Nganyadi <maxwelltechmaster@outlook.com>
2021-03-05 14:30:42 -0600

    made help messages clearer

--- euler2/sum_of_even_fibs.py
+++ euler2/sum_of_even_fibs.py
@@ -6,7 +6,9 @@ import sys
 def create_file(tail):
     head, tail = os.path.split(tail)
     if not os.path.exists(head):
-        sys.exit("{}: Directory does not exist.".format(head))
+        sys.exit(f"{head}: Directory does not exist or is not relative to current directory. "
+                   "Create the directory or use the absolute path. It may also be that the filename "
+                   "doesn't conform to file naming conventions")
     elif tail == '':
         sys.exit("FILE must not end with a slash. The last component of the path should be the filename.")
     tail = tail.split('.')[0]
@@ -19,23 +21,29 @@ def fibonacci(args):
         args.file = create_file(args.file)
     last, current = 0, 1
     print(f"Fib(0) is {last}", f"Fib(1) is {current}", sep='\n', file=args.file)
-    for i in range(2, args.n+1):
-        last, current = current, current+last
-        print(f"Fib({i}) is {current}", file=args.file)
+    if args.max_num is None:
+        for cur_term in range(2, args.max_term+1):
+            last, current = current, current+last
+            print(f"Fib({cur_term}) is {current}", file=args.file)
+    else:
+        cur_term = 2
+        while current > args.max_num:
+            last, current = current, current+last
+            print(f"Fib({cur_term}) is {current}", file=args.file)
     args.file.close()
 
 def nth_fibonacci(args):
-    if args.n == 0:
+    if args.max_term == 0:
         return print(0)
     last, current = 0, 1
-    for i in range(2, args.n+1):
+    for i in range(2, args.max_term+1):
         last, current = current, current+last
     print(current)
 
-def sum_of_even_fib_nums(upperbound):
+def sum_of_fibs(args):
     sum = 0
     last, current = 0, 1
-    while current < upperbound:
+    while current < args.max_term:
         last, current = current, current+last
         if current % 2 == 0:
             sum += current
@@ -46,10 +54,17 @@ if __name__ == "__main__":
     subparsers = parser.add_subparsers()
 
     parser_list = subparsers.add_parser('list', description='Lists all the fibonacci terms, up to F(n)')
-    parser_list.add_argument('n', type=int, help='Is the last fibonacci term that the action will list')
+    parser_list.add_argument('n', type=int, 
+                             help='Depending on if -mt or -mn is selected, this determines the last fibonacci\
+                                   term to go up to, or the biggest fibonacci value to stay below.')
+    group = parser_list.add_mutually_exclusive_group(required=True)
+    group.add_argument('-mt', '--max-term', action='store_true',
+                       help='Uses n to go through all fibonacci values up to the nth term. F(0) to F(n).')
+    group.add_argument('-mn', '--max-num', action='store_true',
+                       help='Uses n to go through all fibonacci values with a value less than n. F(0) to F(?) > n')
     parser_list.add_argument('-f', '--file', type=str, default=sys.stdout,
                              help='Creates text file called FILE. Fibonacci terms will be written to\
-                                   FILE instead of the command line. FILE can be a path, but the last\
+                                   FILE instead of the command line. FILE can be an existing path, but the last\
                                    component of the path will be the filename. Any extensions to FILE\
                                    will be stripped away, the end result will always be FILE.txt.\
                                    WARNING: if FILE already exists, all of its contents will be deleted\
@@ -59,6 +74,10 @@ if __name__ == "__main__":
     parser_nth_fibonacci = subparsers.add_parser('nth_fibonacci', description='Given n, returns F(n).')
     parser_nth_fibonacci.add_argument('n', type=int, help='The fibonacci term you want')
     parser_nth_fibonacci.set_defaults(func=nth_fibonacci)
+
+    parser_sum = subparsers.add_parser('sum', description='Calculates the sum of desired Fibonacci terms')
+    parser_sum.add_argument('n', type=int, help='The range in which to calculate for Fibonacci terms. F(0) to F(n)')
+    parser_sum.set_defaults(func=sum_of_fibs)
 
     args = parser.parse_args()
     args.func(args)
